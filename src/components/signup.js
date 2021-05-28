d300978eb29bdbbd3b857ce85f930b609cd3c607
SimonSkade <simon.skade@gmx.net>
2021-05-28 20:09:46 +0200

    started creating signup page (but really just started)

---
+++ src/components/signup.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { From, Button, Card } from "react-bootstrap";
+
+export default function signup() {
+    return (
+        <>
+            <Card></Card>
+            <div className="w-100 text-center mt-2">
+                Already have an account? Log In    
+            </div>  
+        </>
+    )
+}
