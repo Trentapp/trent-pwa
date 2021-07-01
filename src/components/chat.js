326ef8285320a90d6b3415033d08e72f499c1327
SimonSkade <simon.skade@gmx.net>
2021-07-01 21:57:52 +0200

    little warning fixes etc

--- src/components/chat.js
+++ src/components/chat.js
@@ -10,19 +10,19 @@ const Message = props => {
     return(
         <div className="col-lg-6 offset-3">
         {props.message.sender === props.user._id ? (
-            <div class="d-flex flex-row justify-content-end mb-4">
-              <div class="p-3 me-3 border" style={{borderRadius: "15px", backgroundColor: "#fbfbfb"}}>
-                <p class="small mb-0">{props.message.content}</p>
+            <div className="d-flex flex-row justify-content-end mb-4">
+              <div className="p-3 me-3 border" style={{borderRadius: "15px", backgroundColor: "#fbfbfb"}}>
+                <p className="small mb-0">{props.message.content}</p>
               </div>
               <img src="https://mdbootstrap.com/img/Photos/new-templates/bootstrap-chat/ava2-bg.png" alt="avatar 1" 
                 style={{width: "45px", height: "100%"}} /> {/*replace with profile photo later*/}
             </div>
         ) : (
-            <div class="d-flex flex-row justify-content-start mb-4">
+            <div className="d-flex flex-row justify-content-start mb-4">
               <img src="https://mdbootstrap.com/img/Photos/new-templates/bootstrap-chat/ava1-bg.png" alt="avatar 1"
                 style={{width: "45px", height: "100%"}} />
-              <div class="p-3 ms-3" style={{borderRadius: "15px", backgroundColor: "rgba(57, 192, 237,.2)"}}>
-                <p class="small mb-0">{props.message.content}</p>
+              <div className="p-3 ms-3" style={{borderRadius: "15px", backgroundColor: "rgba(57, 192, 237,.2)"}}>
+                <p className="small mb-0">{props.message.content}</p>
               </div>
             </div>
         )}
@@ -46,7 +46,7 @@ const Chat = props => {
             }
         }
         getChat(props.match.params.id);
-    }, [props.match.params.id])
+    }, [props.match.params.id, props.user._id])
 
     const onSendMessage = async () => { //this is exactly the same code as in product; maybe connect it somehow so I don't need to change everything twice
         try {
@@ -65,7 +65,7 @@ const Chat = props => {
     return(
     <>
         <h2>Chat with {otherUser} because of product {chat.item_id}</h2>
-        {chat.messages && chat.messages.map(message => <Message user={props.user} message={message}/>)}
+        {chat.messages && chat.messages.map(message => <Message user={props.user} message={message} key={message._id}/>)}
         <div className="col-lg-6 offset-3">
             <input type="text" ref={messageRef} />
             <Button onClick={onSendMessage}>Send</Button>
