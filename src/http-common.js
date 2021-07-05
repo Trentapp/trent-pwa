a3ab9391d31ef1aa6b4d933ce39a33356027d947
SimonSkade <simon.skade@gmx.net>
2021-07-05 22:47:00 +0200

    changed routes to make it work in production

--- src/http-common.js
+++ src/http-common.js
@@ -2,13 +2,13 @@ import axios from "axios";
 
 //just returning an axios instance with basic configurations
 export default axios.create({
-    baseURL: "http://localhost:8000/api",
+    baseURL: "http://trentapp.com/api",
     headers: {
         "Content-type": "application/json"
     }
 });
 export const axiosFile = axios.create({
-    baseURL: "http://localhost:8000/api",
+    baseURL: "http://trentapp.com/api",
     headers: {
         "Content-type": "miltipart/form-data"
     } // I think this is standard
