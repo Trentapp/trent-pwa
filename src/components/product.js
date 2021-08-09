605811f7f39c0dfbe94bbf87e00c0a2588657ebe
SimonSkade <simon.skade@gmx.net>
2021-08-09 09:21:46 +0200

    just tested showing an image

--- src/components/product.js
+++ src/components/product.js
@@ -1,13 +1,15 @@
 import React, {useState, useEffect, useRef} from "react";
 import {Link, useHistory} from "react-router-dom";
 import {Button} from "react-bootstrap";
-//import ImageGallery from 'react-image-gallery';
+import ImageGallery from 'react-image-gallery';
+// import sharp from "sharp";
 
 import ProductDataService from "../services/product-data";
 import TransactionDataService from "../services/transaction-data";
 import ChatDataService from "../services/chat-data";
 import BookingRequest from "../components/booking-request";
 import QuestionForm from "../components/ask-question";
+import { Box, Grid, GridItem, Image } from "@chakra-ui/react";
 
 
 const Product = props => {
@@ -103,43 +105,153 @@ const Product = props => {
         getProduct(props.match.params.id);
     }, [props.match.params.id, history]);
 
+    useEffect(() => {
+        try {
+            let imgs = [];
+            if (product.pictures){
+                for (let picture of product.pictures) {
+                    // const base64 = `data:${picture.contentType};base64,${Buffer.from(picture.data.data).toString('base64')}`;
+                    // const image = sharp(Buffer.from(picture.data.data))
+                    //     .resize({height: 500})
+                    //     .toBuffer();
+                    // const imageSm = sharp(Buffer.from(picture))
+                    //     .resize({height: 100})
+                    //     .toBuffer();
+                    // imgs.push({
+                    //     original: `data:${picture.contentType};base64,${image.toString('base64')}`,
+                    //     thumbnail: `data:${picture.contentType};base64,${imageSm.toString('base64')}`
+                    // });
+                    imgs.push({
+                        original: `data:${picture.contentType};base64,${picture.data.data.toString('base64')}`,
+                        thumbnail: `data:${picture.contentType};base64,${picture.data.data.toString('base64')}`
+                    })
+                }
+                //setImages(imgs)
+            }
+        } catch(e) {
+            setError("Could not make images work");
+            console.log("Error in product.js - getProduct: ", e);
+        }
+    }, [product]);
+
+
     return(
-        <div>
-            {error ? <h5>{error}</h5> : (
-            <>
-                <QuestionForm user={props.user} onHide={onHideModal} show={showQuestionForm} onSendMessage={onSendMessage} messageRef={messageRef} />
-                <BookingRequest user={props.user} product={product} onHide={onHideModal} show={showReq} onSendRequest={onSendRequest} startDate={startDate} endDate={endDate} onChangeStartDate={onChangeStartDate} onChangeEndDate={onChangeEndDate}/>
-                <div className="mb-4">
-                    <h2>{product.name}</h2>
-                    <p><b>Price:</b> {product.prices.perDay}€/day {product.prices.perHour && <>or {product.prices.perHour}€/hour</>}</p>
-                    <p><span>Description: </span>{product.desc}</p>
-                    {/*product.pictures && <ImageGallery items={product.pictures.map(picture => {thumbnail})}/> */ /*Maybe use ImageGallery later*/ }
-                    {product.pictures && product.pictures.map(picture => <img height="300" width="auto" alt="" src={`data:image/png;base64,${picture.base64}`}/> )} {/*attention! for fynns pictures I would need that prefix: data:image/png;base64, // Can I use png for jpeg images when I converted them to base64? (probably not)*/}
-                    <br/><br/>
-                    <p>This product belongs to <Link to={`/profile/${product.user._id}`}>{product.user.name}</Link></p>
-                    {props.user._id === product.user._id && (<>
-                        <p><Link to={`/products/update/${product._id}`}>Edit product</Link></p>
-                        <button type="button" className="btn btn-danger" onClick={deleteProduct}>Delete</button>
-                    </>)}
-                </div>
-                <div className="mb-4">
-                    {props.user._id !== product.user._id && props.user._id &&
-                    <>
-                    <div className="float-end">
-                        <Button variant="primary" className="float-end" onClick={onRequestButtonClick}>Request product</Button>
-                    </div>
-                    <br/>
-                    <br/>
-                    <div className="float-end">
-                        <Button variant="primary" className="float-end" onClick={onAskQuestionButtonClick}>Ask Question</Button>
-                    </div>                    
-                    <br/>
-                    </>}
-                </div>
-            </>
-            )}
-        </div>
+        <Box>
+            <Box>
+                {product.name}
+                {product.picturesFitted && <Image src={`data:${product.picturesFitted[2].contentType};base64,${Buffer.from(product.picturesFitted[2].data.data).toString('base64')}`} />}
+            </Box>
+        </Box>
     )
 }
 
 export default Product;
+
+// const ImageGrid = ({ photos, overallRating, ...props }: ImageGridProps) => {
+//     const [isCarouselOpen, setIsCarouselOpen] = useState(false);
+  
+//     const onClose = () => {
+//       setIsCarouselOpen(false);
+//     };
+  
+//     return (
+//       <>
+//         <CarouselModal isOpen={isCarouselOpen} onClose={onClose} />
+  
+//         <Grid
+//           templateColumns="repeat(6, 1fr)"
+//           templateRows="repeat(3, 1fr)"
+//           gap={2}
+//           pos="relative"
+//           borderRadius="xl"
+//           overflow="hidden"
+//           {...props}
+//         >
+//           <GridImage colSpan={4} rowSpan={3} photo={photos[0]}>
+//             <HStack pos="absolute" spacing={2} top={3} left={4}>
+//               <IconButtonOpaque
+//                 aria-label="Share listing"
+//                 icon={<Icon as={FiShare} boxSize={5} />}
+//               />
+//               <IconButtonOpaque
+//                 aria-label="Save listing"
+//                 icon={<Icon as={AiOutlineHeart} boxSize={5} />}
+//               />
+//             </HStack>
+  
+//             <ButtonOpaque
+//               pos="absolute"
+//               bottom={3}
+//               left={4}
+//               rightIcon={<Icon as={AiOutlineStar} boxSize={5} />}
+//             >
+//               {overallRating.toFixed(2)}
+//             </ButtonOpaque>
+//           </GridImage>
+  
+//           <GridImage colSpan={2} rowSpan={1} photo={photos[1]} />
+  
+//           {photos
+//             .slice(2, 6)
+//             .concat(Array(Math.abs(4 - photos.length) || 0).fill(null))
+//             .map((photo, idx) => (
+//               <GridImage key={photo?.id ?? idx} photo={photo} />
+//             ))}
+  
+//           <ButtonOpaque
+//             pos="absolute"
+//             bottom={3}
+//             right={4}
+//             leftIcon={<Icon as={HiOutlinePhotograph} boxSize={5} />}
+//             onClick={() => setIsCarouselOpen((prev) => !prev)}
+//           >
+//             {photos.length}
+//           </ButtonOpaque>
+//         </Grid>
+//       </>
+//     );
+//   };
+
+// const GridImage = props => {
+//     return (
+//       <GridItem overflow="hidden" {...props}>
+//         <Image src={`data:${picture.contentType};base64,${Buffer.from(picture.data.data).toString('base64')}`} />
+//       </GridItem>
+//     );
+// };
+
+/* <div>
+{error ? <h5>{error}</h5> : (
+<>
+    <QuestionForm user={props.user} onHide={onHideModal} show={showQuestionForm} onSendMessage={onSendMessage} messageRef={messageRef} />
+    <BookingRequest user={props.user} product={product} onHide={onHideModal} show={showReq} onSendRequest={onSendRequest} startDate={startDate} endDate={endDate} onChangeStartDate={onChangeStartDate} onChangeEndDate={onChangeEndDate}/>
+    <div className="mb-4">
+        <h2>{product.name}</h2>
+        <p><b>Price:</b> {product.prices.perDay}€/day {product.prices.perHour && <>or {product.prices.perHour}€/hour</>}</p>
+        <p><span>Description: </span>{product.desc}</p>
+        {product.pictures && <ImageGallery items={product.pictures.map(picture => {thumbnail})}/>}
+        {product.pictures && product.pictures.map(picture => <img height="300" width="auto" alt="" src={`data:image/png;base64,${picture.base64}`}/> )} 
+        <br/><br/>
+        <p>This product belongs to <Link to={`/profile/${product.user._id}`}>{product.user.name}</Link></p>
+        {props.user._id === product.user._id && (<>
+            <p><Link to={`/products/update/${product._id}`}>Edit product</Link></p>
+            <button type="button" className="btn btn-danger" onClick={deleteProduct}>Delete</button>
+        </>)}
+    </div>
+    <div className="mb-4">
+        {props.user._id !== product.user._id && props.user._id &&
+        <>
+        <div className="float-end">
+            <Button variant="primary" className="float-end" onClick={onRequestButtonClick}>Request product</Button>
+        </div>
+        <br/>
+        <br/>
+        <div className="float-end">
+            <Button variant="primary" className="float-end" onClick={onAskQuestionButtonClick}>Ask Question</Button>
+        </div>                    
+        <br/>
+        </>}
+    </div>
+</>
+)}
+</div> */
